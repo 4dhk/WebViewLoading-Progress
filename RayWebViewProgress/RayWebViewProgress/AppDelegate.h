18276cfeb4cb384c9c7722c760d32b097db5e625
罗雷雷 <939824240@qq.com>
2017-01-12 16:27:10 +0800

    + init Project.

---
+++ RayWebViewProgress/RayWebViewProgress/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RayWebViewProgress
+//
+//  Created by 罗雷雷 on 2017/1/12.
+//  Copyright © 2017年 4D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RayWebViewProgress/RayWebViewProgress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RayWebViewProgress
+//
+//  Created by 罗雷雷 on 2017/1/12.
+//  Copyright © 2017年 4D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
